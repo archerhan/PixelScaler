46e2e78ff0d1d06702864ac22543bd4e63f8b74a
archerhan <archerhan1990@outlook.com>
2026-04-20 08:15:15 +0800

    Initial Commit

---
+++ PixelScaler/PixelScalerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelScalerApp.swift
+//  PixelScaler
+//
+//  Created by it on 2026/4/20.
+//
+
+import SwiftUI
+
+@main
+struct PixelScalerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
